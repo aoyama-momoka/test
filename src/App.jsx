af0d0b2143d49181e59460689e0acada937bd127
Aoyama Momoka <aoyama_momoka@aoyama-momoka.local>
2022-11-22 17:21:44 +0900

    Add text

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+// import { render } from '@testing-library/react';
+import { useState } from 'react';
+import './App.css';
+
+// テキスト修正
+
+function App({count}) {
+  let [count, setCount] =useState(0);
+  const countPlus =()=>{
+    setCount(count+1);
+    console.log(count);
+  }
+  const countMinus =()=>{
+    setCount(count-1);
+  }
+  return (
+    <div className="App">
+      <p className="number">{count}</p>
+      <button className="btn-plus" onClick={countPlus}>+1</button>
+      <button className="btn-minus" onClick={countMinus}>-1</button>
+    </div>
+  );
+}
+
+
+
+export default App;
